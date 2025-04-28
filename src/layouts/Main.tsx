a1fdff4f05dac014211cc9f62010e2c3738cf53e
shejanNizam <nizam.shejan11@gmail.com>
2025-04-28 09:53:58 +0600

    setup react router with layouts

---
+++ src/layouts/Main.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export default function Main() {
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
---
+++ src/pages/Main/Home/Home/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h3 className=" font-bold text-center text-3xl text-red-600 ">
+        {" "}
+        Hello from home{" "}
+      </h3>
+    </>
+  );
+}
